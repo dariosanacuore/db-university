2cd082423dbdcdb51e1b59f54dd6ac57a04a79c1
dariosanacuore <dariosanacuore2@gmail.com>
2026-01-14 16:23:48 +0100

    feat:Completato l'8 punto

--- queries.sql
+++ queries.sql
@@ -31,3 +31,12 @@ AND date = '2020-06-20';
 SELECT *
 FROM degrees
 WHERE level='magistrale';
+
+--7. Da quanti dipartimenti è composta l'università? (12)
+SELECT *
+FROM departments
+
+--8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+SELECT *
+FROM teachers
+WHERE phone IS NULL;
