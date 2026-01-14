dd839180ccf237e466dc03a1df911e56e4ee0d1c
dariosanacuore <dariosanacuore2@gmail.com>
2026-01-14 16:15:39 +0100

    feat:Completato il 5 punto

--- queries.sql
+++ queries.sql
@@ -19,3 +19,10 @@ SELECT *
 FROM courses
 WHERE year = 1
 AND period = 'I semestre';
+
+--5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del
+--20/06/2020 (21)
+SELECT *
+FROM exams
+WHERE hour > '14:00:00'
+AND date = '2020-06-20';
