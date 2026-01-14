2fd698973749ef1f72d4ae2d0b8dfd4bbd20251f
dariosanacuore <dariosanacuore2@gmail.com>
2026-01-14 16:10:48 +0100

    feat:Completato 4 punto

--- queries.sql
+++ queries.sql
@@ -12,3 +12,10 @@ WHERE cfu>10;
 SELECT *
 FROM students
 WHERE YEAR(date_of_birth) <= YEAR(CURDATE()) - 30;
+
+--4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di
+--laurea (286)
+SELECT *
+FROM courses
+WHERE year = 1
+AND period = 'I semestre';
