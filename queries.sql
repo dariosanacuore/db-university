3c2778077ec991dc8e62d9717e49c4f847f52853
dariosanacuore <dariosanacuore2@gmail.com>
2026-01-15 17:57:36 +0100

    feat:Completato punto 4(JOIN)

--- queries.sql
+++ queries.sql
@@ -85,3 +85,10 @@ FROM courses
 INNER JOIN course_teacher ON course_teacher.course_id = courses.id
 INNER JOIN teachers ON teachers.id = course_teacher.teacher_id
 WHERE teachers.id = 44;
+--4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
+--relativo dipartimento, in ordine alfabetico per cognome e nome
+SELECT students.*,degrees.name,departments.name 
+FROM students
+INNER JOIN degrees ON students.degree_id = degrees.id
+INNER JOIN departments ON degrees.department_id = departments.id
+ORDER BY students.surname, students.name;
