829f9049d15e983f80fbc8eb14373b90b9305a02
dariosanacuore <dariosanacuore2@gmail.com>
2026-01-15 17:55:08 +0100

    feat:Completato punto 3(JOIN)

--- queries.sql
+++ queries.sql
@@ -79,3 +79,9 @@ FROM courses
 INNER JOIN degrees ON degrees.id = courses.degree_id
 INNER JOIN departments ON departments.id = degrees.department_id
 WHERE departments.name='Dipartimento di Neuroscienze'
+--3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT courses.*
+FROM courses
+INNER JOIN course_teacher ON course_teacher.course_id = courses.id
+INNER JOIN teachers ON teachers.id = course_teacher.teacher_id
+WHERE teachers.id = 44;
